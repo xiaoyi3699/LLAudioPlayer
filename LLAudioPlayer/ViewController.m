b88f8465116c1602620d367558a191734402da4a
WangZhaomeng <wangzhaomeng@wangzhaomengdemacbook-pro.local>
2017-04-17 15:46:49 +0800

    Initial Commit

---
+++ LLAudioPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LLAudioPlayer
+//
+//  Created by WangZhaomeng on 2017/4/17.
+//  Copyright © 2017年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
